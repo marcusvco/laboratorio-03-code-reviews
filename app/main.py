528caa9e8f0dfde61c4015f9cbd05ca3f073c874
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2025-10-05 10:58:33 -0300

    feat: fetching pull requests

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 from models.repository import Repository
+from models.pull_request import PullRequest
 from utils.constants import TOTAL_REPOSITORIOS
 
 load_dotenv()
@@ -11,7 +12,7 @@ load_dotenv()
 token = os.getenv("GITHUB_TOKEN")
 headers = {"Authorization": f"Bearer {token}"}
 query = """
-query($queryString: String!, $first: Int!, $after: String) {
+query($queryString: String!, $first: Int!, $after: String, $prFirst: Int!) {
   search(query: $queryString, type: REPOSITORY, first: $first, after: $after) {
     repositoryCount
     pageInfo {
@@ -29,6 +30,28 @@ query($queryString: String!, $first: Int!, $after: String) {
           forkCount
           url
           description
+          pullRequests(first: $prFirst, states: [CLOSED, MERGED], orderBy: {field: CREATED_AT, direction: DESC}) {
+            totalCount
+            nodes {
+              number
+              title
+              createdAt
+              merged
+              closed
+              author {
+                login
+              }
+              reviews(first: 5) {
+                nodes {
+                  author {
+                    login
+                  }
+                  submittedAt
+                  state
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -46,7 +69,12 @@ def fetch_github_repos() -> list[Repository]:
     print(f"Buscando {TOTAL_REPOSITORIOS} repositórios mais populares no GitHub...")
 
     while len(all_repos) < TOTAL_REPOSITORIOS:
-        variables = {"queryString": query_string, "first": first, "after": after_cursor}
+        variables = {
+            "queryString": query_string,
+            "first": first,
+            "after": after_cursor,
+            "prFirst": 1,
+        }
         response = requests.post(
             "https://api.github.com/graphql",
             json={"query": query, "variables": variables},
@@ -74,6 +102,12 @@ def fetch_github_repos() -> list[Repository]:
                 age_delta = datetime.now(timezone.utc) - created_at_date
                 age_years = age_delta.days / 365.25
 
+            pull_requests = []
+            for pr in repo.get("pullRequests", {}).get("nodes", []):
+                pull_requests.append(
+                    PullRequest(url=f"{repo['url']}/pull/{pr['number']}")
+                )
+
             all_repos.append(
                 Repository(
                     name=repo["name"],
@@ -81,6 +115,7 @@ def fetch_github_repos() -> list[Repository]:
                     # maturidade_anos=round(age_years, 2),
                     # atividade_releases=repo["releases"]["totalCount"],
                     url=repo["url"],
+                    pull_requests=pull_requests,
                 )
             )
 
@@ -97,4 +132,5 @@ def fetch_github_repos() -> list[Repository]:
 if __name__ == "__main__":
     repositories = fetch_github_repos()
     for repo in repositories:
+        print("-----")
         print(repo)
--- app/models/repository.py
+++ app/models/repository.py
@@ -1,8 +1,9 @@
 class Repository:
-    def __init__(self, name, url, stars):
+    def __init__(self, name, url, stars, pull_requests):
         self.name = name
         self.url = url
         self.stars = stars
+        self.pull_requests = pull_requests
 
     def __str__(self):
-        return f"Repository(name={self.name}, url={self.url}, stars={self.stars})"
+        return f"Repository(name={self.name}, url={self.url}, stars={self.stars}, pull_requests={self.pull_requests[0]})"
